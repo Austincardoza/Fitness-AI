3cf930e5c2968dee467512e6ad751f98df7cee9a
unknown <aaustincardoza66@gmail.com>
2025-10-22 12:48:41 +0530

    adding m/s and service discovery channels

---
+++ activityservice/src/main/java/com/fitness/activityservice/service/UserValidationService.java
@@ -0,0 +1,37 @@
+package com.fitness.activityservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserValidationService {
+    private final WebClient webClient;
+
+
+    public boolean validateUser(String userId) {
+        log.info("Calling User Validation API for userId: {}", userId);
+
+        try {
+            return webClient.get()
+                    .uri("/api/users/{userid}/validate", userId)
+                    .retrieve()
+                    .bodyToMono(Boolean.class)
+                    .block();
+        }
+        catch (WebClientResponseException e) {
+            if(e.getStatusCode() == HttpStatus.NOT_FOUND)
+                throw new RuntimeException("User Not Found:" + userId);
+            else if(e.getStatusCode() == HttpStatus.BAD_REQUEST)
+                throw new RuntimeException("Invalid Request:" + userId);
+        }
+        return false;
+    }
+}
---
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.fitness.userservice.controller;
+
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@AllArgsConstructor
+public class UserController {
+
+    private UserService userService;
+
+    @GetMapping("/{userid}")
+    public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userid) {
+        return ResponseEntity.ok(userService.getUserProfile(userid));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> registerUser(@Valid @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(userService.register(request));
+    }
+
+
+    @GetMapping("/{userid}/validate")
+    public ResponseEntity<Boolean> validateUser(@PathVariable String userid) {
+        return ResponseEntity.ok(userService.existByUserId(userid));
+    }
+}
