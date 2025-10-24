14925b1dafc677dccd2a0deee7af0508bae69bb4
Austin <austincardoza66@gmail.com>
2025-10-24 12:56:33 +0530

    FE-1, Added AI service with Gemini API

---
+++ aiservice/src/main/java/com/fitness/aiservice/config/RabbitMqConfig.java
@@ -0,0 +1,32 @@
+package com.fitness.aiservice.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Bean
+    public Queue activityQueue() {
+        return new Queue("activity.queue", true);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+     return new DirectExchange("fitness.exchange");
+    }
+
+    @Bean
+    public Binding binding(Queue activityQueue, DirectExchange exchange) {
+        return  BindingBuilder.bind(activityQueue).to(exchange).with("activity.tracking");
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
